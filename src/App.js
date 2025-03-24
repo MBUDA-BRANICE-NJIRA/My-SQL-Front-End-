dd3a2eb831d6e6a8c4eca287600d7559fdc5bf04
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-03-24 11:28:07 +0300

    Forgot to install axios therefore indtalled the axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Login from './Components/LogIn';
+import Registercompo from './Components/Register';
+import Tablecompo from './Components/AllSTudentTable';
+import AddStudent from './Components/AddStudent';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        < Navbar />
+        <Switch>
+
+          <Route path="/LogIn">
+          <Login/>
+
+          </Route>
+          <Route path='/register'>
+          <Registercompo/>
+          </Route>
+
+          <Route path='/AllStudentTable'>
+          <Tablecompo />
+          </Route>
+
+          <Route path='/AddStudent'>
+          <AddStudent />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
